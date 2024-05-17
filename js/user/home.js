9c252f99aa794815fda613f7f20db574a93f9ff0
VF-ROOT\talen001 <nishant.taletiya@vcontractor.co.za>
2024-05-17 23:26:59 +0530

    change

--- js/user/home.js
+++ js/user/home.js
@@ -1,15 +1,34 @@
 function loadHome() {
-
     var mainSection = document.getElementById('content');
-    mainSection.innerHTML=''
+    mainSection.innerHTML = '';
     console.log('home load');
-    var homeContent = `
-        <section id="home" class="content">
-            <img src="./images/userbanner.jpg" width="100%" height="100%" />
-            
-        </section>
+
+    // Create the image element
+    var img = document.createElement('img');
+    img.src = './images/userbanner.jpg';
+    img.className = 'home-image';  // Add a class to the image element
+
+    // Append the image to the main section
+    mainSection.appendChild(img);
+
+    // Create a style element
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    var css = `
+        .home-image {
+            width: 100%;
+            height: 100%;
+            opacity: 0.5; /* Decrease the opacity */
+        }
     `;
 
+    // Add the CSS to the style element
+    if (style.styleSheet) {
+        style.styleSheet.cssText = css;  // For IE
+    } else {
+        style.appendChild(document.createTextNode(css));  // For other browsers
+    }
 
-    mainSection.innerHTML = homeContent;
+    // Append the style element to the document head
+    document.head.appendChild(style);
 }
--- js/usermain.js
+++ js/usermain.js
@@ -1,3 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-    loadLanding()
+    // loadLanding()
+    loadHeader()
+    loadUserNav()
+    loadHome()
 });
