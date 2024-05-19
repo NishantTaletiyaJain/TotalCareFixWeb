c2c58fea9a8031a93545510d3e0af92856b1163e
VF-ROOT\talen001 <nishant.taletiya@vcontractor.co.za>
2024-05-20 02:15:39 +0530

    test

--- js/user/header.js
+++ js/user/header.js
@@ -1,38 +1,3 @@
-// function loadHeader() {
-//     // Dynamically create style element
-//     var styleElement = document.createElement('style');
-//     styleElement.innerHTML = `
-//         header {
-//             background-color: rgb(218, 215, 215);
-//             display: flex;
-//             justify-content: center;
-//             align-items: center;
-//             height: 3rem;
-//             color: black;
-//         }
-//     `;
-    
-//     // Dynamically create script element
-//     var scriptElement = document.createElement('script');
-//     scriptElement.innerHTML = `
-//         function loadUserDashboard() {
-//             // Your code for loading the user dashboard goes here
-//         }
-//     `;
-    
-//     // Append style and script elements to the document head
-//     document.head.appendChild(styleElement);
-//     document.head.appendChild(scriptElement);
-
-//     // Now you can manipulate the header as needed
-//     var headerSection = document.getElementById('head');
-//     var headerContent = `
-//         <h1 class="header-item">Total Care Fix</h1>
-//     `;
-//     headerSection.innerHTML = headerContent;
-// }
-
-
 function loadHeader() {
     // Dynamically create style element
     var styleElement = document.createElement('style');
@@ -54,7 +19,7 @@ function loadHeader() {
     // Now you can manipulate the header as needed
     var headerSection = document.getElementById('head');
     var headerContent = `
-        <h1 class="header-item">Total Care Fix</h1>
+        <h1 class="header-item">Total Care Fix Test</h1>
     `;
     headerSection.innerHTML = headerContent;
 }
